7439f557352c76de326cd3ef641d49bf2f3369d6
JoseFaMG <j.muxozcano.g656@edu.utc.mx>
2023-06-27 08:27:35 -0600

    actualizacion

--- assets/php/registro_usuario_be.php
+++ assets/php/registro_usuario_be.php
@@ -4,9 +4,9 @@
     $nombre_completo = $_POST['nombre_completo'];
     $correo = $_POST['correo'];
     $usuario = $_POST['usuario'];
-    $contracena = $_POST['contacena'];
+    $contracena = $_POST['contracena'];
 
-    $query = "INSERT INTO usarios(nombre_completo, correo, usuario, contracena)
+    $query = "INSERT INTO usuarios(nombre_completo, correo, usuario, contracena)
              VALUES('$nombre_completo','$correo','$usuario','$contracena')";
 
     $ejecutar = mysql_query($conexion, $query);
