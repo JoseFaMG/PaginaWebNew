45f1f72f2b94783cde6afa68f3e19fa877e2322c
JoseFaMG <j.muxozcano.g656@edu.utc.mx>
2023-06-28 02:21:57 -0600

    actualizar

--- php/usuario.php
+++ php/usuario.php
@@ -1,24 +1,21 @@
 <?php
-    session_start();
-    include 'conexionbd.php';
+     
+     include 'conexionbd.php';
 
-    $correo= $_POST['correo'];
-    $contrasena= $_POST['contrasena'];
+     $correo = $_POST['correo'];
+     $contrasena = $_POST['contrasena'];
 
-    
-    $validar_login = mysqli_query($conexion,"SELECT * FROM usuarios WHERE correo ='$correo' and contrasena ='$contrasena'");
-    if(mysqli_num_rows($validar_login)>0){
-        $_SESSION['usuario']=$correo;
-        header("location: ../index.php");
+     $validar_login= mysqli_query($conexion,"SELECT * FROM usuarios WHERE correo='$correo' and contrasena ='$contrasena'");
+
+     if(mysqli_num_rows($validar_login)>0){
+        header("location: ../bienbenido.php");
         exit;
     }else{
-
         echo '<script>
-        alert("El usaurio no existe, favor de verificar sus datos");
-        window.location = "../login.php";
+        alert("Usuario Incorrecto, por favor verifique los datos introducidos");
+        windows.location = "../login.php";
         </script>';
         exit;
-
-    }
+     }
 
 ?>
